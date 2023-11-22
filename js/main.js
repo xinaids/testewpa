f6584eb86c014cd969996e82e5535e07654ee220
jaisson <jaissonduarte@yahoo.com.br>
2023-11-22 16:39:29 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,3 +27,41 @@ window.onload = () => {
  });
  }
  */
+
+function installApp() {
+  // Show the prompt
+  deferredPrompt.prompt();
+  setupButton.disabled = true;
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+          .then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+              console.log('PWA setup accepted');
+              // hide our user interface that shows our A2HS button
+              setupButton.style.display = 'none';
+            } else {
+              console.log('PWA setup rejected');
+            }
+            deferredPrompt = null;
+          });
+}
+
+function registerNotification() {
+  Notification.requestPermission(permission => {
+    if (permission === 'granted') {
+      registerBackgroundSync();
+    } else
+      console.error("Permission was not granted.");
+  });
+}
+
+function registerBackgroundSync() {
+  if (!navigator.serviceWorker) {
+    return console.error("Service Worker not supported");
+  }
+
+  navigator.serviceWorker.ready
+          .then(registration => registration.sync.register('syncAttendees'))
+          .then(() => console.log("Registered background sync"))
+          .catch(err => console.error("Error registering background sync", err));
+}
