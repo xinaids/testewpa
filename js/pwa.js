b8d6296fefd5b6a2f3f40c59ff74ff7ad67ec360
jaisson <jaissonduarte@yahoo.com.br>
2023-11-25 17:38:33 -0300

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -8,14 +8,17 @@ window.onload = () => {
 
 
 
-    const periodicSyncPermission = navigator.permissions.query({name: 'periodic-background-sync'});
-    if (periodicSyncPermission.state == 'granted') {
-      navigator.serviceWorker.ready
-            .then(registration => registration.periodicSync.register('database-sync-periodic', {minInterval: 2}))
-            .then(() => console.log("Serviço de sincronização periódica em segundo plano registrado com sucesso"))
-          .catch(err => console.error("Erro ao registrar o serviço de sincronização periódica em segundo plano", err));
-    } else {
-      console.log('Requisitar periodic-background-sync');
-    }
+
   }
 };
+
+
+const periodicSyncPermission = await navigator.permissions.query({name: 'periodic-background-sync'});
+if (periodicSyncPermission.state == 'granted') {
+  navigator.serviceWorker.ready
+        .then(registration => registration.periodicSync.register('database-sync-periodic', {minInterval: 2}))
+        .then(() => console.log("Serviço de sincronização periódica em segundo plano registrado com sucesso"))
+      .catch(err => console.error("Erro ao registrar o serviço de sincronização periódica em segundo plano", err));
+} else {
+  console.log('Requisitar periodic-background-sync');
+}
