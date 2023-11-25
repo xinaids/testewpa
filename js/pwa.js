4bd62e6dbcd07e52764631c0e8d8e7907b7b8f8f
jaisson <jaissonduarte@yahoo.com.br>
2023-11-25 15:46:15 -0300

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -6,5 +6,6 @@ window.onload = () => {
           .then(() => console.log("Serviço de sincronização em segundo plano registrado com sucesso"))
           .catch(err => console.error("Erro ao registrar o serviço de sincronização em segundo plano", err));
 
+    Notification.requestPermission();
   }
 };
