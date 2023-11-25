f0d140526e3a7217b780918f9baaea7c1e8352ba
jaisson <jaissonduarte@yahoo.com.br>
2023-11-25 16:56:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -83,18 +83,19 @@ function buscarDados(request) {
 function atualizaBD(response) {
   response.json()
           .then(jsonResponse => {
-            
+            console.log(jsonResponse);
+            /*
             self.data.matchAll().then(arrData => {
               console.log(arrData);
-              /*
+              
               data.forEach(client => {
                 client.postMessage(JSON.stringify({
                   type: response.url,
                   data: jsonResponse.data
                 }));
-              });*/
+              });
             });
-            return jsonResponse.data;
+            return jsonResponse.data;*/
           });
   return [1,2];
   /* response.json()
