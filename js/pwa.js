51cd42bed2e558e9898a7b9ab8142e5064f1ba2a
jaisson <jaissonduarte@yahoo.com.br>
2023-11-25 15:32:00 -0300

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -1,10 +1,10 @@
-
-window.onload = async () => {
+window.onload = () => {
   if ('serviceWorker' in navigator) {
-    const rgSW = await navigator.serviceWorker.register('./sw.js');
-    rgSW.ready
-        .then(registration => registration.sync.register('syncAttendees'))
-        .then(() => console.log("Registered background sync"))
-        .catch(err => console.error("Error registering background sync", err));
+    navigator.serviceWorker.register('./sw.js');
+    navigator.serviceWorker.ready
+          .then(registration => registration.sync.register('syncAttendees'))
+          .then(() => console.log("Registered background sync"))
+          .catch(err => console.error("Error registering background sync", err));
+
   }
 };
